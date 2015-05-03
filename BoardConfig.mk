Euphoria-OS/android_device_lge_mako

build kernel inline
Change-Id: Ibf9937e42b7ea97e85af52c633e5eead01598c06

BoardConfig.mk
@@ -30,6 +30,9 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677 user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
+TARGET_KERNEL_SOURCE := kernel/lge/mako
+TARGET_KERNEL_CONFIG := mako_defconfig
+
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO:= false
 BOARD_USES_FLUENCE_INCALL := true

snak3ater/platform_device_lge_mako

BoardConfig.mk
@@ -32,6 +32,9 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 TOUCH_BOOST_DEBUG := false
 
+TARGET_KERNEL_SOURCE := kernel/lge/mako
+TARGET_KERNEL_CONFIG := semaphore_mako_defconfig
+
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO:= false
 BOARD_USES_FLUENCE_INCALL := true